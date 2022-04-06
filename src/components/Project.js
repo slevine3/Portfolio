302506258e27fc59ed5d288e063748c4795d88d4
slevine3 <levinesam3@gmail.com>
2022-04-06 11:33:42 +0300

    added memory game to projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import Climber_Nation from "./Images/climberNation.PNG";
 import Wordle from "./Images/wordle.PNG";
+import MemoryGame from "./Images/MemoryGame.PNG";
 
 export const Project = () => {
   return (
@@ -61,7 +62,7 @@ export const Project = () => {
             <img
               className="project_image"
               src={Wordle}
-              alt="Climber_Nation"
+              alt="Wordle"
             ></img>
 
             <div class="img_overlay image_overlay_blur">
@@ -101,6 +102,55 @@ export const Project = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="all_project_container">
+        <div className="project_display_container">
+       
+          <div className="img_wrap">
+            <img
+              className="project_image"
+              src={MemoryGame}
+              alt="Memory Game"
+            ></img>
+
+            <div class="img_overlay image_overlay_blur">
+              <a href="https://slevine3.github.io/MemoryGame/" className="img_title">
+              MemoryGame
+              </a>
+              <a href="https://slevine3.github.io/MemoryGame/" className="img_description">
+              Fully functioning memory game built with vanilla Javascript
+              </a>
+            </div>
+          </div>
+
+          <div className="project_links">
+            <div>
+              <a
+                className="project_subtitle"
+                href="https://slevine3.github.io/MemoryGame/"
+              >
+                <h3>Live Site</h3>
+              </a>
+            </div>
+            <div>
+              <a
+                className="project_subtitle"
+                href="https://www.loom.com/share/bb705a51d5fb4327a4950593590ee2ff"
+              >
+                <h3>Demo Video</h3>
+              </a>
+            </div>
+            <div>
+              <a
+                className="project_subtitle"
+                href="https://github.com/slevine3/MemoryGame"
+              >
+                <h3>Source Code</h3>
+              </a>
+            </div>
+          </div>
+        </div>
+       
       </div>
     </div>
   );
