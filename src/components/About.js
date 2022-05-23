07b89bfedcb2cd68632d54eed021798a5cf5fe0b
slevine3 <levinesam3@gmail.com>
2022-05-23 13:40:01 +0300

    update location

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export const About = () => {
       </div>
       <div className="about_body">
         <div className="about_description">
-          I'm a software engineer based out of Haifa, Israel with a love for
+          I'm a software engineer based out of Tzrufa, Israel with a love for
           surfing, rock climbing, logic, and technology. I recently graduated
           from Developers Institute; an internationally recognized coding
           bootcamp where I had the opportunity to sharpen my skills as a
