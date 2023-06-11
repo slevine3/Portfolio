986da22b0c47f15e42b1f95c5e134dbb15ee70f1
Sam Levine <sam.l@shoesonline.co.il>
2023-06-11 11:54:36 +0300

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,13 @@ export const About = () => {
         <div className="about_description">
           I'm Sam Levine, a web developer based in Tzrufa, Israel. With
           expertise in technologies like React, Node.js, and MongoDB, I
-          specialized in building scalable and user-friendly web applications.
+          specialize in building scalable and user-friendly web applications.
           <br></br>
           <br></br>
-          In my previous role, I worked as a Full Stack Web Developer at
+          Currently, I work as a Full Stack Web Developer at
           Shoesonline, where I developed an intuitive admin panel using React
-          with TypeScript and Redux. I also maintained microservices
-          architecture and optimized communication between components.
+          with TypeScript and Redux. I also maintain microservices
+          architecture and optimize communication between components.
           <br></br>
           <br></br>I hold a degree in Economics from Queens College and completed a prestigious full-stack web development program
           at Developers Institute.
